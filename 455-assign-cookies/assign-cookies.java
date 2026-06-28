e2d1cc344bce5487e691f55fb13a495b14b87994
yaseenans008 <yaseenans008@gmail.com>
2026-06-29 04:05:53 +0530

    Time: 15 ms (61.68%) | Memory: 51.5 MB (55.97%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        int n1=g.length;
+        int n2=s.length;
+        int count =0;
+        while(i<n1&&j<n2){
+            if(s[j]>=g[i]){
+                i++;
+                j++;
+                count++;
+            }
+            else{
+                j++;
+            }
+        }
+        return count;
+    }
+}
