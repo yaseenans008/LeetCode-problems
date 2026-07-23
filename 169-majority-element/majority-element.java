736007b3369355cc5a9ba4d269c50c8786292563
yaseenans008 <yaseenans008@gmail.com>
2026-07-24 00:11:17 +0530

    Time: 2 ms (70.38%) | Memory: 55.8 MB (17.99%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ans=0;
+        int freq=0;
+        for(int i=0;i<nums.length;i++){
+            if(freq==0) ans=nums[i];
+            if(ans== nums[i]) freq ++;
+            else freq--;
+
+        }
+        return ans;
+    }
+}
