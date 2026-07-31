a146b5649923859ae20aaa6e518c3df20d7c34cf
yaseenans008 <yaseenans008@gmail.com>
2026-07-31 17:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (36.77%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -5,7 +5,12 @@ class Solution {
        while(exclusive!=0){
         if((exclusive & 1)==1) count++;
         exclusive=exclusive>>1;
-       } 
+       }
+        /* we can also use count=0;
+        while(exclusive!=0){
+        exclusive=exclusive&(exclusive-1) // this makes rightmost bit set to 0 
+        count ++ ;
+        } */
        return count;
     }
 }
